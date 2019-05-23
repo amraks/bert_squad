3c0826c6a00080fb6eeffb328c5c29641ea4bf56
Harsh Savla <harsh.savla@riverbed.com>
2019-05-23 14:48:41 -0700

    read chapter

--- convert_to_squad.py
+++ convert_to_squad.py
@@ -1,9 +1,26 @@
 import os
+import csv
 
 def get_test_file_for_chapter(tests_dir, chapter_file_name):
      base, ext = chapter_file_name.replace('token', 'test').split('.')
      test_file_name = base + '_strip' + '.' + ext
      return os.path.join(tests_dir, test_file_name)
+
+def get_chapter_number(chapter_file):
+    return os.path.basename(chapter_file).split('_ch')[1].split('_')[0]
+
+def process_chapter(chapter_file, test_file):
+    with open(chapter_file) as chapter_fh, open(test_file) as test_fh:
+        chapter_csv_reader = csv.reader(chapter_fh)
+        test_csv_reader = csv.reader(test_fh)
+
+        chapter_number = get_chapter_number(chapter_file)
+
+        print('chapter %s' % chapter_number)
+        for row in chapter_csv_reader:
+            values = [s.strip() for s in row if s]
+            print(values)
+
 
 def main():
     chapters_dir = os.path.join(os.path.abspath('.'), "process_chapter")
@@ -14,9 +31,9 @@ def main():
             continue
 
         test_file = get_test_file_for_chapter(tests_dir, chapter_file_name)
-        chapter_file_csv = os.path.join(chapters_dir, chapter_file_name)
+        chapter_file = os.path.join(chapters_dir, chapter_file_name)
 
-        print('ch_file:%s test_file:%s' % (chapter_file_csv, test_file))
+        process_chapter(chapter_file, test_file)
 
 if __name__ == '__main__':
     main()
